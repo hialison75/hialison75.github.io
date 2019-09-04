82ed94139d7d167a1cb8f0cdbd8dd4671535d982
hialison75 <54887436+hialison75@users.noreply.github.com>
2019-09-05 02:23:53 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = '軒丞超愛我, ' + myName;
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = '軒丞超愛我, ' + storedName;
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
